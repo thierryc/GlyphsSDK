ec263420ac2787c13e34b4f544c8de83ebf859e0
schriftgestalt <georg.seifert@mac.com>
2024-12-19 00:19:19 +0100

    add sample code

---
+++ Python Samples/Document exported/Document exported callback.py
@@ -0,0 +1,18 @@
+# MenuTitle: Document Exported Callback
+# -*- coding: utf-8 -*-
+
+__doc__ = """"""
+
+from GlyphsApp import Glyphs, DOCUMENTEXPORTED
+from Cocoa import NSObject
+
+def exportCallback(info):
+	try:
+		print(info.object())
+	except:
+		# Error. Print exception.
+		import traceback
+		print(traceback.format_exc())
+
+# add your function to the hook
+Glyphs.addCallback(exportCallback, DOCUMENTEXPORTED)
